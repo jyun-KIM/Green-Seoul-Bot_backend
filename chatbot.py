2a9bd797f2a86f9f7085f7167b37acb74901f29e
jyun27 <kimjyun27@gmail.com>
2024-08-20 17:59:56 +0900

    first commit, processing Image

--- chatbot.py
+++ chatbot.py
@@ -4,8 +4,13 @@ import openai
 import os
 from werkzeug.utils import secure_filename
 from config import logger
-from generate_chatbot import load_docs, create_vectorstore, create_rag_chain
+from generate_chatbot import load_rewardPolicy, create_vectorstore, processing_user_input,  processing_image
+
 import json
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage
+import base64
+import httpx
 
 
 app = Flask(__name__)
@@ -21,15 +26,33 @@ Chatbot = Namespace('Chatbot')
 logger.info("Application started!")
 
 # OpenAI GPT-4o 응답 생성 함수    
-def get_response(user_input):
+def get_response1(user_input):
     try:
         # 문서 로드 및 벡터 스토어 생성
-        documents = load_docs() 
+        documents = load_rewardPolicy() 
         vectorstore = create_vectorstore(documents)
 
         # RetrievalQA 체인 생성
-        qa_chain = create_rag_chain(vectorstore)
+        qa_chain = processing_user_input(vectorstore)
         answer = qa_chain.invoke({"input": user_input})
+
+
+        return answer
+    
+    except Exception as e:
+        print(f"Error fetching response from OpenAI: {e}")
+        return "죄송합니다. 현재 서비스를 제공할 수 없습니다. 나중에 다시 시도해 주세요."
+    
+
+    # OpenAI GPT-4o 응답 생성 함수    
+def get_response_img():
+    try:
+        answer = processing_image()
+
+        print("cc")
+        #print(answer)
+
+        #print(answer.content)
 
         return answer
     
@@ -52,7 +75,7 @@ class TestAPI(Resource):
         if not user_input:
             return jsonify({"message": "입력해주세요."}), 400
 
-        bot_response = get_response(user_input)
+        bot_response = get_response_img(user_input)
 
         # API 응답 반환
         return jsonify({"message": bot_response}), 200
@@ -81,40 +104,25 @@ def allowed_file(filename):
 
 # 사진 업로드 처리
 @Chatbot.route('/upload')
-class UploadPhoto(Resource):
+class Chat(Resource):
     def post(self):
-        """사진 업로드 처리"""
-        try:
-            # 파일이 요청에 포함되어 있는지 확인
-            if 'image_file' not in request.files:
-                return jsonify({"message": "파일이 첨부되지 않았습니다."}), 400
-            
-            file = request.files['image_file']
-
-            # 파일 이름이 있는지 확인
-            if file.filename == '':
-                return jsonify({"message": "파일 이름이 비어 있습니다."}), 400
-
-            # 파일이 허용된 형식인지 확인
-            if file and allowed_file(file.filename):
-                filename = secure_filename(file.filename)
-                file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-                
-                # 디렉토리가 없으면 생성
-                os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-                
-                # 파일 저장
-                file.save(file_path)
-
-                # 파일 업로드 성공 메시지 반환 (예: 인식된 물체 이름을 반환)
-                recognized_result = "can"  # 여기에 실제 인식 결과를 넣을 수 있습니다.
-                return jsonify({"message": f"The object recognized is a {recognized_result}"}), 200
-            else:
-                return jsonify({"message": "허용되지 않는 파일 형식입니다."}), 400
+        """사용자 입력처리
+        data = request.get_json()
+        print(data)
+        user_input = data['user_input']
         
-        except Exception as e:
-            logger.error(f"Error during file upload: {e}", exc_info=True)  # 에러 로그 추가
-            return jsonify({"message": "서버 오류가 발생했습니다.", "error": str(e)}), 500
+        if not user_input:
+            return jsonify({"message": "입력해주세요."}), 400
+    
+        """
+
+        bot_response = get_response_img()
+        print("bot_response",bot_response, "finish bot_response")
+        #answer = bot_response['answer']
+
+        return jsonify({"message": bot_response})
+
+            
 
 # 정책 정보 조회
 @Chatbot.route('/policy')
@@ -138,14 +146,12 @@ class Policy(Resource):
         '''
 
     # ChatGPT를 통해 응답 생성
-        bot_response = get_response(district_name)
+        bot_response = get_response1(district_name)
         message = bot_response['answer']
       
         
         # 정책 정보와 홈페이지 링크를 결합하여 반환
         homepage_url = next(item["district_url"] for item in district_websites if item["district_name"] == district_name)
-        
-        #gpt가 만든 답변은 필요 없음. message :"송파구"만 있으면 +url 
         
         return jsonify({"message": message, "homepage_url": homepage_url})
     
@@ -155,14 +161,15 @@ class Chat(Resource):
     def post(self):
         """사용자 입력처리"""
         data = request.get_json('user_input')
-        print(data)
+        #print(data)
         user_input = data['user_input']
         
 
         if not user_input:
             return jsonify({"message": "입력해주세요."}), 400
     
-        bot_response = get_response(user_input)
+        bot_response = get_response1(user_input)
+        print(bot_response)
         answer = bot_response['answer']
 
         return jsonify({"message": answer})
@@ -171,3 +178,4 @@ class Chat(Resource):
 
 if __name__ == '__main__':
     app.run(debug=True)
+
